2ecbb922d5ceda9f5e3ea2116814684169810107
MihPar <9496260@gmail.com>
2023-05-18 16:44:45 +0300

    one

---
+++ script01.js
@@ -0,0 +1,4 @@
+let dog = {
+  name: "Mars",
+  numLegs: 4
+};
---
+++ script07.js
@@ -0,0 +1,10 @@
+function Dog(name, color) {
+    this.name = name,
+    this.color = color,
+    this.numLegs = 4
+  }
+  
+  let terrier = new Dog("Mars", "black")
+  terrier.name
+  terrier.color
+  terrier.numLegs
